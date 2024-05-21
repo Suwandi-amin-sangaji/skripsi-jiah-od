473e47fff771d06f61226a2a93a69e98e6235e92
Suwandi-amin-sangaji <suwandiaminsangaji@gmail.com>
2024-05-21 21:27:31 +0900

    model

--- src/main.js
+++ src/main.js
@@ -2,6 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.css'
-// import 'material-icons/iconfont/filled.css';
+import 'material-icons/iconfont/filled.css';
 
 createApp(App).mount('#app')
--- src/plugins/ObjectDetection.js
+++ src/plugins/ObjectDetection.js
@@ -11,8 +11,8 @@ export default class ObjectDetection {
         this.model = null
         this.classes = null
 
-        this.model_url = model_url;
-        this.classes_url = classes_url;
+        this.model_url = model_url
+        this.classes_url = classes_url
     }
 
     isModelLoaded = () => {
@@ -23,37 +23,25 @@ export default class ObjectDetection {
     }
 
     loadModel = async () => {
-        try {
-            this.model = await tf.loadGraphModel(this.model_url);
-            console.log('Model loaded successfully');
-        } catch (error) {
-            console.error('Error loading model:', error);
-        }
+        this.model = await Object.freeze(
+            tf.loadGraphModel(this.model_url)
+        );
     }
-
-
     loadClasses = async () => {
-        try {
-            const response = await fetch(this.classes_url);
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-            this.classes = await response.json();
-            console.log('Classes loaded successfully');
-        } catch (error) {
-            console.error('Error loading classes:', error);
-        }
+        this.classes = await fetch(this.classes_url)
+            .then((r) => r.json())
+        return this.classes
+
     }
 
     detect = async (image, detection_threshold = 0.5) => {
         if (!this.isModelLoaded()) {
-            console.error('Model not yet loaded');
-            return undefined;
+            console.error('Model not yet loaded')
+            return undefined
         }
-
         if (!this.isClassesLoaded()) {
-            console.error('Classes not yet loaded');
-            return undefined;
+            console.error('Classes are empty')
+            return undefined
         }
 
         const detection = await this.model.executeAsync(
@@ -81,37 +69,26 @@ export default class ObjectDetection {
     _buildDetectedObjects = ({ scores, threshold, boxes, classes, classesDir, frame }) => {
         const detectionObjects = [];
 
-        // Check if scores is an array and has data
-        if (Array.isArray(scores) && scores.length > 0) {
-            // Assuming scores is a 2D array with scores in the first row
-            const scoreArray = scores[0];
+        scores[0].forEach((score, i) => {
+            if (score > threshold) {
+                const bbox = [];
+                const minY = boxes[0][i][0] * frame.naturalHeight;
+                const minX = boxes[0][i][1] * frame.naturalWidth;
+                const maxY = boxes[0][i][2] * frame.naturalHeight;
+                const maxX = boxes[0][i][3] * frame.naturalWidth;
 
-            // Iterate over the scores
-            scoreArray.forEach((score, i) => {
-                if (score > threshold) {
-                    const bbox = [];
-                    const minY = boxes[0][i][0] * frame.naturalHeight;
-                    const minX = boxes[0][i][1] * frame.naturalWidth;
-                    const maxY = boxes[0][i][2] * frame.naturalHeight;
-                    const maxX = boxes[0][i][3] * frame.naturalWidth;
+                bbox[0] = minX;
+                bbox[1] = minY;
+                bbox[2] = maxX - minX;
+                bbox[3] = maxY - minY;
 
-                    bbox[0] = minX;
-                    bbox[1] = minY;
-                    bbox[2] = maxX - minX;
-                    bbox[3] = maxY - minY;
-
-                    detectionObjects.push({
-                        class: classesDir[classes[i]],
-                        score: score.toFixed(4),
-                        bbox: bbox,
-                    });
-                }
-            });
-        } else {
-            console.error('Invalid scores format:', scores);
-        }
-
+                detectionObjects.push({
+                    class: classesDir[classes[i]],
+                    score: score.toFixed(4),
+                    bbox: bbox,
+                });
+            }
+        });
         return detectionObjects;
     }
-
 }
